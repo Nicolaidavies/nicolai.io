303973efcdd3bda3e6331277f5c26c41f2c30457
Nicolai Davies <nicolaidavies.nd@gmail.com>
2017-12-12 14:25:31 +0200

    Footer

--- src/App.vue
+++ src/App.vue
@@ -2,15 +2,18 @@
   <div id="app">
     <Navigation />
 
-    <router-view/>
+    <router-view />
+
+    <Footer />
   </div>
 </template>
 
 <script>
   import Navigation from './components/Navigation.vue'
+  import Footer from './components/Footer.vue'
   export default {
     name: 'app',
-    components: {Navigation}
+    components: {Navigation, Footer}
   }
 </script>
 
---
+++ src/components/Footer.vue
@@ -0,0 +1,8 @@
+<template>
+  <p class="footer">&copy; Nicolai Davies 2017</p>
+</template>
+<style scoped>
+  .footer {
+    text-align: center;
+  }
+</style>
