959fa8fc233cd46fbc424880c34187b8d8000b36
Nicolai Davies <nicolaidavies.nd@gmail.com>
2018-01-17 11:09:53 +0100

    Navigation is now responsive

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -1,14 +1,14 @@
 <template>
   <nav class="navigation">
-    <router-link to="/projects" active-class="active">
+    <router-link to="/projects" class="link" active-class="active">
       Projects
     </router-link>
 
-    <router-link to="/" exact active-class="active">
+    <router-link to="/" exact class="link" active-class="active">
       <i class="fa fa-home" aria-hidden="true"></i>
     </router-link>
 
-    <router-link  to="/resume" active-class="active">
+    <router-link  to="/resume" class="link" active-class="active">
       Resume
     </router-link>
   </nav>
@@ -21,25 +21,36 @@
 </script>
 
 <style scoped>
-  nav {
+  .navigation {
+    max-width: 600px;
+    margin: 0 auto;
+    display: flex;
+    flex-direction: row;
+
     padding: 2em 0;
     text-align: center;
   }
 
-  .active {
-    color: black !important;
-  }
-
-  a {
-    display: inline;
-    padding: 0 1em;
+  .link {
+    flex: 1;
     color: #9E9E9E;
     text-decoration: none;
     font-size: 1.5em;
   }
 
-  a:hover {
+  .link:hover, .link.active {
     color: #424242;
     text-decoration: underline;
+  }
+
+  @media (max-width: 300px) {
+    .navigation {
+      flex-direction: column;
+      padding: 1em 0;
+    }
+
+    .link {
+      padding: 0.25em 0;
+    }
   }
 </style>
