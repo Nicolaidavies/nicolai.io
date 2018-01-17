93a6698bcef3b465593bb4f5f09cfffe07ace31c
Nicolai Davies <nicolaidavies.nd@gmail.com>
2018-01-17 14:19:59 +0100

    Cleaner urls for projects

---
+++ pages/project/custom.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <h1 style="text-align: center">I am an example of a completely custom page</h1>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'waterford',
+    components: {Project},
+    data() {
+      return {}
+    },
+    mounted() {
+      console.log('rendering custom comp')
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
