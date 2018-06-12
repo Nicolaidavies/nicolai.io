a2704d18ec2c6dac0e512e50cb9536753549a243
Nicolai Davies <Nicolai.d@hotmail.com>
2018-06-12 13:17:35 +0200

    Google Site stuff

--- nuxt.config.js
+++ nuxt.config.js
@@ -7,7 +7,8 @@ module.exports = {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: 'Nicolai Davies, I am a software developer' }
+      { hid: 'description', name: 'description', content: 'Nicolai Davies, I am a software developer' },
+      { name: 'google-site-verification', content: 'huDKm3IBMy5SiMUWAqhzqsgUMd59PO--GFMcN3gd8Dw'}
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
