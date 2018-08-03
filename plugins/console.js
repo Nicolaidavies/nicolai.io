6a454f67b0aa471eac5184f739462a684c9a8d7f
Nicolai Davies <Nicolai.d@hotmail.com>
2018-08-03 13:58:04 +0200

    simpler console

--- plugins/console.js
+++ plugins/console.js
@@ -2,7 +2,6 @@ export default () => {
   var base22MainStyle = "font-size:34px; font-weight:200; letter-spacing:0.02em; line-height:1.4em; font-family:helvetica,arial; color:rgba(0,0,0,0.9);";
   var base22MessageStyle = "font-size:21px; font-weight:200; letter-spacing:0.2em; line-height:1.4em; font-family:helvetica,arial; color:rgba(0,0,25,0.5);";
 
-  console.log("%cHi, you seem curious", base22MainStyle)
-  console.log("%cI built this site with Nuxt.js, in case you were wondering.", base22MessageStyle)
+  console.log("%cHello", base22MainStyle)
   console.log("%cIf you would like to work with me, you should send me an email: mail@nicolai.io", base22MessageStyle)
 }
