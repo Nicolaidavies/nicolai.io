6632e8b132f324f20e6a76f27469c1767ad42aae
Nicolai Davies <Nicolai.d@hotmail.com>
2018-08-28 15:29:41 +0200

    Source code link in console

--- plugins/console.js
+++ plugins/console.js
@@ -2,6 +2,6 @@ export default () => {
   var base22MainStyle = "font-size:34px; font-weight:200; letter-spacing:0.02em; line-height:1.4em; font-family:helvetica,arial; color:rgba(0,0,0,0.9);";
   var base22MessageStyle = "font-size:21px; font-weight:200; letter-spacing:0.2em; line-height:1.4em; font-family:helvetica,arial; color:rgba(0,0,25,0.5);";
 
-  console.log("%cHello", base22MainStyle)
-  console.log("%cIf you would like to work with me, you should send me an email: mail@nicolai.io", base22MessageStyle)
+  console.log("%cPsst", base22MainStyle)
+  console.log("%cThe source code for this site is available on github: https://github.com/nicolaidavies/nicolai.io", base22MessageStyle)
 }
